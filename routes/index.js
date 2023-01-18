fd09a213d98cbc3e560ebee868f673519e5d0114
manishksah94@gmail.com <manishksah94@gmail.com>
2023-01-18 17:51:00 +0530

    Added Post router and controller in the app

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
-const funController = require("../controllers/home_controller");
 
 console.log("router loded");
 
-//router.get("/", homeController.home);
-router.get("/", homeController.fun);
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
 
 module.exports = router;
